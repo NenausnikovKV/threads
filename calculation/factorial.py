4c22ee1d7d81973cfff000b91c9403c2d03993a1
Konstantin Nenausnikov <konstantin2113@mail.ru>
2024-08-22 17:41:19 +0300

    add factorial calculation

---
+++ calculation/factorial.py
@@ -0,0 +1,48 @@
+"""Check threat for """
+import time
+import threading
+
+
+class Factorial:
+    """Calculate factorial used one or two threads"""
+
+    def __init__(self, fact_num):
+        self.fact_num = fact_num
+
+    def one_thread_factorial(self):
+        """Get factorial used one thread"""
+        thread = threading.Thread(target=self._factorial)
+        thread.start()
+        thread.join()
+
+    def two_factorials_by_two_threads(self):
+        """Get 2 factorials used two threads"""
+        thread_1 = threading.Thread(target=self._factorial)
+        thread_2 = threading.Thread(target=self._factorial)
+        thread_1.start()
+        thread_2.start()
+        thread_1.join()
+        thread_2.join()
+
+    def _factorial(self, start_num=None):
+        """Calculate factorial"""
+        if start_num is None:
+            start_num = 1
+        fact = 1
+        for n in range(start_num, self.fact_num+1):
+            fact *= n
+        return fact
+
+
+if __name__ == '__main__':
+    factorial = Factorial(fact_num=100000)
+    # One thread processing
+    start_time = time.perf_counter()
+    factorial.one_thread_factorial()
+    end_time = time.perf_counter()
+    print(f"One thread processing time is {end_time - start_time:0.2f} seconds")
+    # Two thread and two factorials processing
+    start_time = time.perf_counter()
+    factorial.two_factorials_by_two_threads()
+    end_time = time.perf_counter()
+    print(f"Two thread processing time is {start_time - end_time:0.2f} seconds")
